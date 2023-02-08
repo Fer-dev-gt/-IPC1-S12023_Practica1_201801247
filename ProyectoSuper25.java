24640356cf7c3c74172f00a946edc6855eb790af
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-02-08 08:17:54 -0600

    Informacion extra

--- ProyectoSuper25.java
+++ ProyectoSuper25.java
@@ -2,7 +2,7 @@ package proyectosuper25;
 import java.util.Scanner;                                                                      // Libreria para guardar inputs de tipo texto
 import static java.lang.Integer.parseInt;                                                      // Libreria para castear datos de tipo String a tipo int
 /**
- * @author fernandoorozco
+ * @author fernandoorozcoaa
  */
 public class ProyectoSuper25 {
         static Scanner entrada = new Scanner(System.in);                                  // Creamos objeto global "entrada" tipo Scanner 
